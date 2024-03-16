1d5be85851bdbbca7f51ae275821d5b5746705e7
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2024-03-16 18:11:18 -0400

    Format code

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -11,38 +11,34 @@ const Aside = () => {
       <div className="header horizontal-line">
         <Header />
       </div>
-      <div className='info-columns'>
-  
-      <div className="infos horizontal-line">
-        {mockData.infos.map((info) => {
-          return <InfosCard info={info} key={info.id} />;
-        })}
-      </div>
-  
-     
-      <div className="socials horizontal-line">
-        <div className="card-title">Socials</div>
-        {mockData.socials.map((info) => {
-          return <InfosCard info={info} key={info.id} />;
-        })}
-      </div>
-  
+      <div className="info-columns">
+        <div className="infos horizontal-line">
+          {mockData.infos.map((info) => {
+            return <InfosCard info={info} key={info.id} />;
+          })}
+        </div>
 
-      <div className="languages">
-        <div className="card-title">Languages</div>
-        {mockData.languages.map((item) => {
-          return <LanguageCard item={item} key={item.id} />;
-        })}
-      </div>
+        <div className="socials horizontal-line">
+          <div className="card-title">Socials</div>
+          {mockData.socials.map((info) => {
+            return <InfosCard info={info} key={info.id} />;
+          })}
+        </div>
 
-      <div className="hobbies">
-        <div className="card-title">Hobbies & Interests</div>
-        {mockData.hobbies.map((item) => {
-          return <HobbiesCard item={item} key={item.id} />;
-        })}
-      </div>
+        <div className="languages">
+          <div className="card-title">Languages</div>
+          {mockData.languages.map((item) => {
+            return <LanguageCard item={item} key={item.id} />;
+          })}
+        </div>
 
-    </div>
+        <div className="hobbies">
+          <div className="card-title">Hobbies & Interests</div>
+          {mockData.hobbies.map((item) => {
+            return <HobbiesCard item={item} key={item.id} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
